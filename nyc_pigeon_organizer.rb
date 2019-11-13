bb3e7c78df6792c10b227d29f3d3fcb064909213
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-13 20:49:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,11 @@ def nyc_pigeon_organizer(pigeon_original_data)
         #if pigeon_data_restructured[name] == nil
         #  pigeon_data_restructured[name] = {}
         #end
-        pigeon_data_restructured[name] = {} if pigeon_data_restructured[name]==nil
-        if pigeon_data_restructured[name][first_level_keys] == nil
-          pigeon_data_restructured[name][first_level_keys] = []
-        end
+        pigeon_data_restructured[name] = {} if (pigeon_data_restructured[name]==nil)
+        #if pigeon_data_restructured[name][first_level_keys] == nil
+        #  pigeon_data_restructured[name][first_level_keys] = []
+        #end
+        pigeon_data_restructured[name][first_level_keys] = [] if (pigeon_data_restructured[name][first_level_keys]==nil)
         pigeon_data_restructured[name][first_level_keys].push(second_level_keys.to_s)
       end
     end 
